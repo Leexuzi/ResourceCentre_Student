e810c8945f956a28dfec9fac90c9d20350b6c2c6
Wayne <22000210@myrp.edu.sg>
2023-07-14 15:26:54 +0800

    updated addChromebook in ResourceCentre, and testAddChromebook in ResourceCentreTest

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -68,6 +68,28 @@ public class ResourceCentreTest {
 	public void testAddChromebook() {
 		//fail("Not yet implemented");
 		// write your code here 
+		assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
+		assertEquals("Test that the Chromebook arraylist is empty.", 0, chromebookList.size());
+		//Given an empty list, after adding 1 item, the size of the list is 1
+		ResourceCentre.addChromebook(chromebookList, cb1);		
+		assertEquals("Test that the Chromebook arraylist size is 1.", 1, chromebookList.size());
+
+		
+		// Add an item
+		ResourceCentre.addChromebook(chromebookList, cb2);
+		assertEquals("Test that the Chromebook arraylist size is now 2.", 2, chromebookList.size());
+		//The item just added is as same as the last item in the list
+		assertSame("Test that Camcorder is added to the end of the list.", cb2, chromebookList.get(1));
+
+		// Add an item that already exists in the list
+		ResourceCentre.addChromebook(chromebookList, cb2);
+		assertEquals("Test that the Chromebook arraylist size is unchange.", 2, chromebookList.size());
+
+		// Add an item that has missing detail
+		Chromebook cb_missing = new Chromebook("CB0014", "", "");
+		ResourceCentre.addChromebook(chromebookList, cb_missing);
+		assertEquals("Test that the Chromebook arraylist size is unchange.", 2, chromebookList.size());
+		
 	}
 
 	@Test
@@ -110,7 +132,7 @@ public class ResourceCentreTest {
 	@Test
 	public void testRetrieveAllChromebook() {
 		//fail("Not yet implemented");
-		// write your code here
+		// write your code heres
 	}
 
 	@Test
